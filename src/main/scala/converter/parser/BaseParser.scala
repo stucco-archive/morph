3f7fa8e83d648fc5690a8e1e3b99c06720feacdb
Anish Athalye <aathalye@me.com>
2013-07-02 15:47:54 -0400

    Removed extraneous method in BaseParser

--- src/main/scala/converter/parser/BaseParser.scala
+++ src/main/scala/converter/parser/BaseParser.scala
@@ -24,17 +24,6 @@ abstract class BaseParser extends Parser with AstBuilder {
     *
     * @return The root of the generated AST.
     */
-  override def apply(input: String): ValueNode = apply(input.toCharArray)
-
-  /** The main parsing method.
-    *
-    * Uses a ReportingParseRunner (which only reports the first error)
-    * for simplicity.
-    *
-    * @param input The content to parse.
-    *
-    * @return The root of the generated AST.
-    */
   override def apply(input: Array[Char]): ValueNode = {
     val parsingResult = ReportingParseRunner(RootRule).run(input)
     parsingResult.result getOrElse {
