78bc5443942e5c7852d37c26c842b140942a3082
Anish Athalye <aathalye@me.com>
2013-07-16 16:52:22 -0400

    Updated sbt config with instructions on build dep

--- project/MorphBuild.scala
+++ project/MorphBuild.scala
@@ -5,6 +5,11 @@ object MorphBuild extends Build {
   lazy val root = Project("morph", file("."))
     .dependsOn(jsonjava)
 
+  // This has to be the customized JSON-java that is hosted
+  // on GitHub at anishathalye/JSON-java
+  // and mirrored at stucco/JSON-java
+  //
+  // The required version is tagged 'morph'
   lazy val jsonjava = GitHub("anishathalye", "JSON-java", "morph")
 
   def GitHub(user: String, project: String, tag: String = null) =
